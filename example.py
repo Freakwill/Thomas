37ac4d23f64fc25e32c54672cd0866122e4dba60
William Song <30965609+Freakwill@users.noreply.github.com>
2018-10-06 21:24:36 +0800

    Add files via upload

--- example.py
+++ example.py
@@ -4,13 +4,17 @@
 
 import pandas as pd
 import numpy as np
+
+# To get some scores, it requires mystat
 from mystat import *
 
-train = pd.read_excel('test.xlsx')
+train = pd.read_excel('test.xlsx') # DataFrame
 
+# data for train
 x_train = train.iloc[:7, 1:]
 y_train = train.iloc[:7, 0]
 
+# data for test
 x_test = train.iloc[7:, 1:]
 y_test = train.iloc[7:, 0]
 
@@ -18,9 +22,13 @@ import thomas
 
 def nb():
     
+    # so easy
     nbc = thomas.NaiveBayesClassifier.fromDataFrame(x_train, y_train)
-
+    
+    # predict
     y_pred = [nbc.predict(x) for x in x_test.values]
+
+    # calculate scores
     scores = check(y_test, y_pred)
     print(scores)
     print('f-score(p)', fscore(*scores))
